731cc6ea27febff9e2c283f66c6b639336596bb5
Dick <shengwangzhong@hengtainsoft.com>
2021-04-14 14:08:48 +0800

    springboot admin

---
+++ Spring Boot：SpringBoot Admin Simple/demo/spring-boot-admin-client/src/main/java/com/dick/AdminClientApplication.java
@@ -0,0 +1,15 @@
+package com.dick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shengwangzhong
+ */
+@SpringBootApplication
+public class AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientApplication.class, args);
+    }
+}
